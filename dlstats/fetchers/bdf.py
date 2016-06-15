61a1aa14c644a9454b8614ae3d7fe22d43eb046f
Menandalbee <menandfrance@163.com>
2016-06-15 11:22:03 +0200

    Unify the coding-style

--- dlstats/fetchers/bdf.py
+++ dlstats/fetchers/bdf.py
@@ -6,63 +6,63 @@ Created on Tue May 24 12:33:35 2016
 """
 
 from lxml import etree
-import xlrd
 import requests
-import pprint
-import sys
-
+import xlrd
 import re
+import sys
+import pprint
+
 from collections import defaultdict,OrderedDict
 
 from dlstats.utils import Downloader, get_ordinal_from_period, make_store_path,get_datetime_from_period
 from dlstats.fetchers._commons import Fetcher, Datasets, Providers, Categories
 
 def make_xls_url(dataset_code):
-    node='DATASETS_'+dataset_code
-    url='http://webstat.banque-france.fr/en/browseExplanation.do?node=%s' % (node)
-    req=requests.get(url)
-    page=req.content
-    html=etree.HTML(page)
-    anchors=html.xpath('.//a[starts-with(@href,"javascript:")]')
-    v=anchors[0].get('href')
-    req=re.match('.*\(\'(.*)\',\'(.*)\',\'(.*)\'\).*',v)
-    url='http://webstat.banque-france.fr/en/exportDsd.do?datasetId=%s&datasetName=%s&keyFamily=%s&node=%s' % (req.group(1),req.group(2),req.group(3),node)
+    node = "DATASETS_" + dataset_code
+    url = "http://webstat.banque-france.fr/en/browseExplanation.do?node=%s" % (node)
+    req = requests.get(url)
+    page = req.content
+    html = etree.HTML(page)
+    anchors = html.xpath('.//a[starts-with(@href,"javascript:")]')
+    v = anchors[0].get('href')
+    req = re.match(r".*\(\'(.*)\',\'(.*)\',\'(.*)\'\).*",v)
+    url = "http://webstat.banque-france.fr/en/exportDsd.do?datasetId=%s&datasetName=%s&keyFamily=%s&node=%s" % (req.group(1),req.group(2),req.group(3),node)
     return url
     
-def get_sheet_cells(fp,name):
-    index=fp.sheet_by_name(name)
-    keys=index.col_values(0,3)
-    values=index.col_values(1,3)
-    d=OrderedDict(zip(keys,values))
+def get_sheet_cells(fp, name):
+    index = fp.sheet_by_name(name)
+    keys = index.col_values(0,3)
+    values = index.col_values(1,3)
+    d = OrderedDict(zip(keys,values))
     return d
 
 def get_concepts(fp):
-    return get_sheet_cells(fp,'index')
+    return get_sheet_cells(fp, 'index')
 
-def get_codelists(fp,dimension_keys):
-    codelists=dict((k,{}) for k in dimension_keys) 
+def get_codelists(fp, dimension_keys):
+    codelists = dict((k,{}) for k in dimension_keys) 
     for k in dimension_keys:
-        codelists[k]=get_sheet_cells(fp,k)
+        codelists[k] = get_sheet_cells(fp,k)
     return codelists
        
 def get_ns(fp):
     '''Get the namespace like urn:sdmx:org.sdmx.infomodel.datastructure.DataStructure=BDF:BDF_AME1(1.0)'''
-    nsmap=list()
-    context=etree.iterparse(fp,events=['start-ns'],remove_blank_text=True)
+    nsmap = list()
+    context = etree.iterparse(fp, events=['start-ns'], remove_blank_text=True)
     for event,elem in context:
         nsmap.append(elem)
-        s='{'+nsmap[-1][1]+'}'
+        s = '{' + nsmap[-1][1] + '}'
     return s
     
-def get_events(fp,ns):
+def get_events(fp, ns):
     '''Build the iterator of events'''
-    context=etree.iterparse(fp,events=['end'],tag=(ns+'Group',ns+'Series',ns+'Obs'),remove_blank_text=True)
+    context = etree.iterparse(fp, events=['end'], tag=(ns + 'Group',ns + 'Series',ns + 'Obs'), remove_blank_text=True)
     return context
     
-def get_dates(dim,obs):
-    frequency=dim['FREQ']
-    start_date=obs[0]['TIME_PERIOD']
-    end_date=obs[-1]['TIME_PERIOD']
+def get_dates(dim, obs):
+    frequency = dim['FREQ']
+    start_date = obs[0]['TIME_PERIOD']
+    end_date = obs[-1]['TIME_PERIOD']
     return frequency,start_date,end_date
     
         
@@ -74,22 +74,22 @@ class BDF_Data:
         self.provider_name = self.dataset.provider_name
         self.dataset_code = self.dataset.dataset_code
         self.release_date = self.dataset.last_update
-        self.dimension_list=self.dataset.dimension_list
-        self.attribute_list=self.dataset.attribute_list
-        self.store_path=self.get_store_path()
+        self.dimension_list = self.dataset.dimension_list
+        self.attribute_list = self.dataset.attribute_list
+        self.store_path = self.get_store_path()
         
-        xls_handle=xlrd.open_workbook(self._load_xls())
-        self.dataset.concepts=get_concepts(xls_handle)
-        self.dataset.dimension_keys=self.dataset.concepts.keys()
-        self.dataset.codelists=get_codelists(xls_handle,self.dataset.dimension_keys)
+        xls_handle = xlrd.open_workbook(self._load_xls())
+        self.dataset.concepts = get_concepts(xls_handle)
+        self.dataset.dimension_keys = self.dataset.concepts.keys()
+        self.dataset.codelists = get_codelists(xls_handle, self.dataset.dimension_keys)
         
-        self.filepath=self._load_datas()       
-        self.nsString=get_ns(self.filepath)
-        self.context=None
-        self.file_handle=None
+        self.filepath = self._load_datas()       
+        self.nsString = get_ns(self.filepath)
+        self.context = None
+        self.file_handle = None
         
-        self.nbseries=0
-        self.nbIteration=0
+        self.nbseries = 0
+        self.nbIteration = 0
 
     def get_store_path(self):
         return make_store_path(base_path=self.fetcher.store_path,
@@ -104,12 +104,12 @@ class BDF_Data:
         return filepath 
     
     def _load_xls(self):
-        url_xls=make_xls_url(self.dataset_code)
+        url_xls = make_xls_url(self.dataset_code)
         download = Downloader(url=url_xls, 
-                          filename='info_'+self.dataset_code,
+                          filename='info_' + self.dataset_code,
                           store_filepath=self.store_path,
                           use_existing_file=self.fetcher.use_existing_file)
-        filepath=download.get_filepath()
+        filepath = download.get_filepath()
         return filepath
     
     def __iter__(self):
@@ -119,76 +119,78 @@ class BDF_Data:
         '''try...except... for closing the file when an error occurs'''
         try:
             if not self.context:
-                self.file_handle=open(self.filepath,'rb')
-                self.context=get_events(self.file_handle,self.nsString)
+                self.file_handle = open(self.filepath, 'rb')
+                self.context = get_events(self.file_handle, self.nsString)
     			
-            self.nbIteration+=1
+            self.nbIteration += 1
             for event,elem in self.context:
-                if elem.tag==self.nsString+'Group':
-                    group=OrderedDict((k,v) for k,v in elem.attrib.items())
-                    obs=list()
-                    self.nbseries+=1
+                if elem.tag == self.nsString + 'Group':
+                    group = OrderedDict((k,v) for k,v in elem.attrib.items())
+                    obs = list()
+                    self.nbseries += 1
                     elem.clear()
-                elif elem.tag==self.nsString+'Obs':
+                elif elem.tag == self.nsString + 'Obs':
                     obs.append(OrderedDict((k,v) for k,v in elem.attrib.items()))
                     elem.clear()
-                elif elem.tag==self.nsString+'Series':
-                    p_series=OrderedDict((k,v) for k,v in elem.attrib.items())               
+                elif elem.tag == self.nsString + 'Series':
+                    p_series = OrderedDict((k,v) for k,v in elem.attrib.items())               
                     elem.clear()
                     break          
-            if self.nbIteration==self.nbseries:
-                series=self.clean_field(self._build_series(group,p_series,obs))        
+            if self.nbIteration == self.nbseries:
+                series = self.clean_field(self._build_series(group, p_series, obs))        
         except:
             self.file_handle.close()
-            print('The iteration stopped because of an error!',sys.exc_info()[1])
+            print("The iteration stopped because of an error!", sys.exc_info()[1])
             raise StopIteration()
         
-        if self.nbIteration!=self.nbseries:
+        if self.nbIteration != self.nbseries:
             self.file_handle.close()
-            print('The iteration is finished!')
+            print("The iteration is finished!")
             raise StopIteration()    
         return series
     
     def clean_field(self, bson):
-        if not "start_ts" in bson or not bson.get("start_ts"):
-            bson["start_ts"] = get_datetime_from_period(bson['start_date'], freq=bson["frequency"])
-        if not "end_ts" in bson or not bson.get("end_ts"):
-            bson["end_ts"] = get_datetime_from_period(bson['end_date'], freq=bson["frequency"])  
+        if not 'start_ts' in bson or not bson.get('start_ts'):
+            bson['start_ts'] = get_datetime_from_period(bson['start_date'], freq=bson['frequency'])
+        if not 'end_ts' in bson or not bson.get('end_ts'):
+            bson['end_ts'] = get_datetime_from_period(bson['end_date'], freq=bson['frequency'])  
         return bson   
                         
-    def _build_series(self,group,p_series,obs):
-        dimensions=OrderedDict()
-        attributes=OrderedDict()
-        bson=OrderedDict() 
-        dim=group.copy()
+    def _build_series(self, group, p_series, obs):
+        dimensions = OrderedDict()
+        attributes = OrderedDict()
+        bson = OrderedDict() 
+        dim = group.copy()
         dim.update(p_series) 
-        attrib=defaultdict(list)
+        attrib = defaultdict(list)
         
-        attribute_keys=[a for a in obs[0].keys() if a not in ['TIME_PERIOD','OBS_VALUE']]
+        attribute_keys = [a for a in obs[0].keys() if a not in ['TIME_PERIOD','OBS_VALUE']]
         
-        frequency,start_date,end_date=get_dates(dim,obs)
+        frequency,start_date,end_date = get_dates(dim, obs)
         self.dataset.add_frequency(frequency)
         
         values=list()
         for v in obs:
-            period=v['TIME_PERIOD']
-            a=OrderedDict()
+            period = v['TIME_PERIOD']
+            a = OrderedDict()
             for k in attribute_keys:
                 try:
-                    a[k]=v[k]
+                    a[k] = v[k]
                 except KeyError:
-                    a[k]=''
+                    a[k] = ''
                 attrib[k].append(a[k])
-            value={ 'attributes': a,
+            value = { 'attributes': a,
                     'release_date': self.release_date,
-                    'ordinal': get_ordinal_from_period(period,freq=frequency),
+                    'ordinal': get_ordinal_from_period(period, freq=frequency),
                     'period': period,
                     'value': v['OBS_VALUE']
                     }
             values.append(value)
         
         for key in self.dataset.dimension_keys:
-            dimensions[key] = self.dimension_list.update_entry(key,dim[key],self.dataset.codelists[key][dim[key]])
+            dimensions[key] = self.dimension_list.update_entry(key,
+                                                                dim[key], 
+                                                                self.dataset.codelists[key][dim[key]])
 #            if not key in self.dataset.codelists:
 #                self.dataset.codelists[key] = {}
 #            if not dimensions[key] in self.dataset.codelists[key]:
@@ -196,17 +198,19 @@ class BDF_Data:
             
         for key in attribute_keys:
             try:
-                attributes[key]=self.attribute_list.update_entry(key,str(attrib[key]),self.dataset.codelists[key][attrib[key]])
+                attributes[key] = self.attribute_list.update_entry(key,
+                                                                str(attrib[key]),
+                                                                self.dataset.codelists[key][attrib[key]])
             except KeyError:
                 pass
-#            if not key in self.dataset.codelists:
-#                self.dataset.codelists[key] = {}
-#            if not attributes[key] in self.dataset.codelists[key]:
-#                self.dataset.codelists[key][str(attributes[key])] = attributes[key]
+            if not key in self.dataset.codelists:
+                self.dataset.codelists[key] = {}
+            if not attributes[key] in self.dataset.codelists[key]:
+                self.dataset.codelists[key][str(attributes[key])] = attributes[key]
             
         
-        series_name=dimensions['EXT_TITLE']
-        series_key=self.nbseries
+        series_name = dimensions['EXT_TITLE']
+        series_key =  self.nbseries
     
         bson['values'] = values                
         bson['provider_name'] = self.provider_name       
@@ -223,26 +227,26 @@ class BDF_Data:
 
 
 '''Test'''
-if __name__=="__main__":
-    f=Fetcher(provider_name='BDF',is_indexes=False)
-    dataset=Datasets(provider_name='BDF',
+if __name__ == "__main__":
+    f = Fetcher(provider_name='BDF',is_indexes=False)
+    dataset = Datasets(provider_name='BDF',
                      dataset_code='DET',
                      name='1234',
-                     doc_href='http://webstat.banque-france.fr/en/concepts.do?node=DATASETS',
+                     doc_href="http://webstat.banque-france.fr/en/concepts.do?node=DATASETS" ,
                      last_update='20160603',
                      fetcher=f,
                      is_load_previous_version=False)
 
-    iterator=BDF_Data(dataset,'http://webstat.banque-france.fr/en/export.do?node=DATASETS_DET&exportType=sdmx')         
+    iterator = BDF_Data(dataset, "http://webstat.banque-france.fr/en/export.do?node=DATASETS_DET&exportType=sdmx")         
     for i in iterator:
         for key in i.keys():
-            print(key,':',i[key])
+            print(key, ':', i[key])
     print()
-    pp=pprint.PrettyPrinter(indent=4)
+    pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(iterator.dataset.dimension_keys)
     print()
     pp.pprint(iterator.dataset.codelists)
-    
+
 
 
                         
