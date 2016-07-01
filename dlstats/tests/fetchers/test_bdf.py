916b5d93faf55dce161a6f17a1f8a5d1ed1ae969
Menandalbee <menandfrance@163.com>
2016-07-01 15:47:02 +0200

    Test update

--- dlstats/tests/fetchers/test_bdf.py
+++ dlstats/tests/fetchers/test_bdf.py
@@ -19,7 +19,7 @@ from dlstats.tests.fetchers.base import BaseFetcherTestCase
 BDF_HTML_PAGES = [
     ("http://webstat.banque-france.fr/en/concepts.do?node=DATASETS", "WebstatBDF.html"),
     ("http://webstat.banque-france.fr/en/browseExplanation.do?node=DATASETS_AME", "AME_page_info.html"),
-    ("http://webstat.banque-france.fr/en/browseExplanation.do?node=DATASETS_DET", "DET_page_info.html")
+    ("http://webstat.banque-france.fr/en/browseExplanation.do?node=DATASETS_ECOFI", "ECOFI_page_info.html")
 ]
 
 RESOURCES_DIR = os.path.abspath(os.path.join(BASE_RESOURCES_DIR, "bdf"))
@@ -135,8 +135,8 @@ DATA_AME = {
     }
 }
 
-DATA_DET = {
-    "filepath": get_filepath("DET.xml"),
+DATA_ECOFI = {
+    "filepath": get_filepath("ECOFI_update.xml"),
     "DSD": {
         "filepath": None,
         "dataset_code": "DET",
@@ -148,105 +148,84 @@ DATA_DET = {
         "concept_keys": [
             'freq',
             'ref-area',
-            'esa95-sector',
-            'esa95-account',
-            'mufa-valuation',
-            'count-area',
-            'data-type-bop',
-            'esa95tp-denom',
-            'obs-status',
-            'obs-conf'
+            'indicator',
+            'counterpart_area',
+            'data_domain',
+            'obs-status'
         ],
         "codelist_keys": [
             'freq',
             'ref-area',
-            'esa95-sector',
-            'esa95-account',
-            'mufa-valuation',
-            'count-area',
-            'data-type-bop',
-            'esa95tp-denom',
-            'obs-status',
-            'obs-conf'
+            'indicator',
+            'counterpart_area',
+            'data_domain',
+            'obs-status'
         ],
         "codelist_count": {   
-            "concept": 10,
+            "concept": 6,
         },                
         "dimension_keys": [
-            'freq',
+            'data-domain',
             'ref-area',
-            'esa95-sector',
-            'esa95-account',
-            'mufa-valuation',
-            'count-area',
-            'data-type-bop',
-            'esa95tp-denom',
+            'indicator',
+            'counterpart-area',
+            'freq'
         ],
         "dimension_count": {
-            "freq": 8,
-            "ref-area": 644,
-            'esa95-sector': 29,
-            'esa95-account': 22,
-            'mufa-valuation': 4,
-            'count-area': 644,
-            'data-type-bop': 10,
-            'esa95tp-denom': 33
+            "data-domain": 8,
+            'ref-area': 644,
+            'indicator': 38,
+            'counterpart-area': 644,
+            'freq': 8
         },
         "attribute_keys": [
             'obs-status',
-            'obs-conf'
         ],
         "attribute_count": {
-            'obs-status': 1,
-            'obs-conf':1
+            'obs-status': 3,
         }
     },
     "series_accept": 4,
     "series_reject_frequency": 0,
     "series_reject_empty": 0,
-    "series_all_values": 119,
-    "series_key_first": "DET.A.FR.11C2.F51100.M.Z9.8.F",
-    "series_key_last": "DET.A.FR.1315.F33200.M.Z9.8.F",
+    "series_all_values": 2033,
+    "series_key_first": "ECOFI.SPI.BY.FID_PA._Z.D",
+    "series_key_last": "ECOFI.SPI.BY.FPE_IX._Z.D",
     "series_sample": {
         'provider_name': 'BDF',
-        'dataset_code': 'DET',
-        'key': 'DET.A.FR.11C2.F51100.M.Z9.8.F',
-        'name': "Percentage of the capital of CAC 40 resident companies held by non-residents",
-        'frequency': 'A',
+        'dataset_code': 'ECOFI',
+        'key': 'ECOFI.SPI.BY.FID_PA._Z.D',
+        'name': "3-month EURIBOR (annual interest rate)",
+        'frequency': 'D',
         'last_update': None,
         'first_value': {
-            'value': '36.0',
-            'ordinal': 29,
-            'period': '1999',
-            'period_o': '1999',
+            'value': '0.055',
+            'ordinal': 16468,
+            'period': '2015-02-02',
             'attributes': {
                 'obs-status': "A",
-                'obs-conf': "F"
             },
         },
         'last_value': {
-            'value': '45.3',
-            'ordinal': 44,
-            'period': '2014',
-            'period_o': '2014',
+            'value': '-0.283',
+            'ordinal': 16979,
+            'period': '2016-06-27',
             'attributes': {
                 'obs-status': "A",
-                'obs-conf': "F"
             },
         },
         'dimensions': {
-            "freq": 8,
-            "ref-area": 'fr',
-            'esa95-sector': '11c2',
-            'esa95-account': 'f51100',
-            'mufa-valuation': 'm',
-            'count-area': 'z9',
-            'data-type-bop': '8',
-            'esa95tp-denom': 'f'
+            'data-domain': 'spi',
+            'ref-area': 'by',
+            'indicator': 'fid-pa',
+            'counterpart-area': 'z',
+            'freq': 'd'
+
         },
         'attributes': None
     }
 }
+
 
 class FetcherTestCase(BaseFetcherTestCase): 
     FETCHER_KLASS = Fetcher
@@ -273,6 +252,18 @@ class FetcherTestCase(BaseFetcherTestCase):
     def _load_files_dataset_ame(self):
         url = "http://webstat.banque-france.fr/en/export.do?node=DATASETS_AME&exportType=sdmx"
         self.register_url(url, self.DATASETS['AME']['filepath'], content_type='text/html')
+		
+    def _load_files_info_ecofi(self):
+        url = "http://webstat.banque-france.fr/en/exportDsd.do?datasetId=189&datasetName=ECOFI&keyFamily=ECOFIN_DSD-EcoFin&node=DATASETS_ECOFI"
+        filepath = get_filepath('ECOFI_info.xls')
+        self.assertTrue(os.path.exists(filepath)) 
+        self.register_url(url, filepath, content_type='text/html')
+		
+    def _load_files_dataset_ecofi_update(self):
+        url1 = "http://webstat.banque-france.fr/en/export.do?node=UPDATES34381&exportType=sdmx"
+        filepath = get_filepath('ECOFI_update.xml')
+        self.assertTrue(os.path.exists(filepath))
+        self.register_url(url1, filepath, content_type='text/html') 
                           
     @httpretty.activate
     @unittest.skipUnless('FULL_TEST' in os.environ, 'Skip-no full test')
@@ -286,11 +277,11 @@ class FetcherTestCase(BaseFetcherTestCase):
     @httpretty.activate     
     @unittest.skipUnless('FULL_TEST' in os.environ, "Skip - no full test")
     def test_load_datasets_update(self):
-        dataset_code = "AME"
+        dataset_code = 'ECOFI'
         self._load_files()
-        self._load_files_info_ame()
-        self._load_files_dataset_ame()
-        self.assertLoadDatasetsUpdate([dataset_code])
+        self._load_files_info_ecofi()
+        self._load_files_dataset_ecofi_update()
+        self.assertLoadDatasetsUpdate([dataset_code])	
 
     @httpretty.activate     
     def test_build_data_tree(self):
@@ -308,33 +299,3 @@ class FetcherTestCase(BaseFetcherTestCase):
         self.assertProvider()
         self.assertDataset(dataset_code)        
         self.assertSeries(dataset_code)
-    
-    @httpretty.activate
-    def test_upsert_dataset_det(self):
-        dataset_code = 'DET'
-        self._load_files()
-        
-        url_info = 'http://webstat.banque-france.fr/en/exportDsd.do?datasetId=156&datasetName=DET&keyFamily=BDF_DET1-Non-Resident Holdings of French Government Negociable Debt Securities&node=DATASETS_DET'
-        self.register_url(url_info, get_filepath('DET_info.xls'), content_type='text/html')
-        
-        url = 'http://webstat.banque-france.fr/en/export.do?node=DATASETS_DET&exportType=sdmx'
-        self.register_url(url, self.DATASETS[dataset_code]['filepath'], content_type='text/html')
-        
-        self.assertProvider()
-        self.assertDataset(dataset_code)        
-        self.assertSeries(dataset_code)
-	
-	@httpretty.activate
-    def test_parse_agenda(self):
-        url = "http://webstat.banque-france.fr/en/ajax/calendarPublication.do"
-        self.register_url(url, get_filepath('calendarPublication.xml'), content_type='text/html')
-        
-        model = [{'dataflow_key': '10',
-                  'reference_period': '2016May',
-                  'scheduled_date': '2016-06-27'},
-                  {'dataflow_key': '3',
-                   'reference_period': '2016May',
-                   'scheduled_date': '2016-06-27'}]
-                 
-        self.assertEqual(list(self.fetcher._parse_agenda())[0], model[0])
-        self.assertEqual(list(self.fetcher._parse_agenda())[1], model[1])
